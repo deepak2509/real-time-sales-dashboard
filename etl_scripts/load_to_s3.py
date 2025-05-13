bb16b5f99e28fb1cc7684b5d7c2652e4ba7ccd3a
deepak2509 <155398117+deepak2509@users.noreply.github.com>
2025-05-13 18:38:23 -0500

    Update load_to_s3.py

--- etl_scripts/load_to_s3.py
+++ etl_scripts/load_to_s3.py
@@ -3,7 +3,6 @@ import boto3
 import pandas as pd
 import io
 
-# AWS credentials (development only — secure appropriately in production)
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = 'us-east-2'
